fd9dfcbea3180a87b64f4e5056dcabbc62ab02cc
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-05-24 10:42:34 +0530

    Wowel charc

---
+++ src/com/bridgelab/basiccoreproblem/WowelCharc.java
@@ -0,0 +1,14 @@
+package com.bridgelab.basiccoreproblem;
+
+import java.util.Scanner;
+
+public class WowelCharc {
+    public static void main(String[] args) {
+        char ch = 'i';
+        if (ch == 'a' || ch == 'b' || ch == 'i' || ch == 'o' || ch == 'u') {
+            System.out.println("is vomel");
+        } else {
+            System.out.println("is consonant");
+        }
+    }
+}
