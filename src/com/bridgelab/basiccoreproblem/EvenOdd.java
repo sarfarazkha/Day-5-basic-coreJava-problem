743772357c431a0e0f62f1d87b363f96c9d3e68a
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-05-24 10:40:50 +0530

    Swaping

--- src/com/bridgelab/basiccoreproblem/EvenOdd.java
+++ src/com/bridgelab/basiccoreproblem/EvenOdd.java
@@ -16,4 +16,3 @@ public class EvenOdd {
         }
     }
 }
-© 202
---
+++ src/com/bridgelab/basiccoreproblem/Swaping.java
@@ -0,0 +1,19 @@
+package com.bridgelab.basiccoreproblem;
+
+import java.util.Scanner;
+
+public class Swaping {
+    public static void main(String[] args) {
+        int x, y, t;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of x and y");
+        x = sc.nextInt();
+        y = sc.nextInt();
+        System.out.println("before swaping number:" + x + " " + y);
+        t = x;
+        x = y;
+        y = t;
+        System.out.println("after swapping:" + x + " " + y);
+    }
+
+}
