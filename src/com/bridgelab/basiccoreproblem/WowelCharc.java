04f52609eb71effdedbdf4101fd0fae6ceadb9d4
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2023-01-11 22:37:23 +0530

    Added: check wovel character

--- src/com/bridgelab/basiccoreproblem/WowelCharc.java
+++ src/com/bridgelab/basiccoreproblem/WowelCharc.java
@@ -2,7 +2,7 @@ package com.bridgelab.basiccoreproblem;
 
 import java.util.Scanner;
 
-public class WowelCharc {
+public class WowelCharc { 
     public static void main(String[] args) {
         char ch = 'i';
         if (ch == 'a' || ch == 'b' || ch == 'i' || ch == 'o' || ch == 'u') {
